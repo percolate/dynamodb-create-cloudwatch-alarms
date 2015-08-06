bf0ade7a9f2d4474d83226663b116004ad636047
mpavlisin <mihailo@percolate.com>
2015-08-06 14:29:59 -0400

    adding main files

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+setup(
+    name='dynamodb-create-cloudwatch-alarms',
+    version='1.1',
+    description='AWS EC2 Security Groups dump tool',
+    url='https://github.com/percolate/dynamodb-create-cloudwatch-alarms',
+    author='Mihailo Pavlisin',
+    author_email='mihailo@percolate.com',
+    license='GPLv3',
+    keywords='aws dynamodb alarms cloudwatch boto',
+    packages=['dynamodb-create-cloudwatch-alarms'],
+    install_requires=[
+        'boto',
+        'docopt'
+    ],
+    entry_points={
+        'console_scripts': [
+            ('dynamodb-create-cloudwatch-alarms='
+             'dynamodb-create-cloudwatch-alarms.main:main')
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: System :: Systems Administration"
+    ]
+)
