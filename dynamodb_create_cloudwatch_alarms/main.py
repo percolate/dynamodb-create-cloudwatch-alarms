0d97dc7e02889450ea7a957ae3fceb91ba4c2f0f
mpavlisin <mihailo@percolate.com>
2016-09-29 14:12:30 -0400

    DDB Alarms about throttled events not actual consumed requests

--- dynamodb_create_cloudwatch_alarms/main.py
+++ dynamodb_create_cloudwatch_alarms/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 """dynamodb-create-cloudwatch-alarms
 
-Script used to create above 80% Read/Write Capacity Units
+Script that creates Read/Write ThrottleEvents
 AWS CloudWatch alarms for each DynamoDB table.
-If set as a cron job - updates existing alarms if
-Read/Write Capacity Units DynamoDB table parameters changed.
-
+Can be set as a cron job.
 
 Usage:
     dynamodb-create-cloudwatch-alarms [options] <sns_topic_arn> <region>
@@ -27,10 +25,11 @@ from constants import VERSION
 
 DEBUG = False
 
-DDB_METRICS = frozenset([u'ConsumedReadCapacityUnits',
-                         u'ConsumedWriteCapacityUnits'])
+DDB_METRICS = frozenset([u'WriteThrottleEvents',
+                         u'ReadThrottleEvents'])
+
 ALARM_PERIOD = 300
-ALARM_EVALUATION_PERIOD = 12
+ALARM_EVALUATION_PERIOD = 3
 
 
 def get_ddb_tables(region):
@@ -48,16 +47,13 @@ def get_ddb_tables(region):
     ddb_connection = boto.dynamodb.connect_to_region(region)
     ddb_tables_list = ddb_connection.list_tables()
     ddb_tables = set()
+
     for ddb_table in ddb_tables_list:
 
         ddb_table_attributes = ddb_connection.describe_table(ddb_table)
         # creating a variable for each unit to satisfy flake8
         ddb_tablename = ddb_table_attributes[u'Table'][u'TableName']
-        ddb_rcu = (ddb_table_attributes
-                   [u'Table'][u'ProvisionedThroughput'][u'ReadCapacityUnits'])
-        ddb_wcu = (ddb_table_attributes
-                   [u'Table'][u'ProvisionedThroughput'][u'WriteCapacityUnits'])
-        ddb_tables.add((ddb_tablename, ddb_rcu, ddb_wcu))
+        ddb_tables.add(ddb_tablename)
 
     return ddb_tables
 
@@ -70,7 +66,7 @@ def get_existing_alarm_names(aws_cw_connect):
         aws_cw_connect (CloudWatchConnection)
 
     Returns:
-        (dict) Existing CloudWatch DDB alarms (name and threshold)
+        (set) Existing CloudWatch DDB alarms (name)
     """
     assert isinstance(aws_cw_connect,
                       boto.ec2.cloudwatch.CloudWatchConnection)
@@ -89,20 +85,18 @@ def get_existing_alarm_names(aws_cw_connect):
         for alarm in page_loop:
             existing_alarms.add(alarm)
 
-    existing_alarm_names = {}
+    existing_alarm_names = set()
 
     for existing_alarm in existing_alarms:
         if existing_alarm.namespace == u'AWS/DynamoDB':
-            existing_alarm_names.update({existing_alarm.name:
-                                         existing_alarm.threshold})
+            existing_alarm_names.add(existing_alarm.name)
 
     return existing_alarm_names
 
 
 def get_ddb_alarms_to_create(ddb_tables, aws_cw_connect, sns_topic_arn):
     """
-    Creates a Read/Write Capacity Units alarm
-    for all DynamoDB tables
+    Creates a Read/Write ThrottleEvents alarms for all DynamoDB tables
 
     Args:
         ddb_tables (set) ist of all DynamoDB tables
@@ -110,8 +104,7 @@ def get_ddb_alarms_to_create(ddb_tables, aws_cw_connect, sns_topic_arn):
         sns_topic_arn (str)
 
     Returns:
-        (set) All new Read/Write Capacity Units alarms that'll be created
-        (set) All existing Read/Write Capacity Units alarms that'll be updated
+        (set) All new Read/Write ThrottleEvents alarms that'll be created
     """
     assert isinstance(ddb_tables, set)
     assert isinstance(aws_cw_connect,
@@ -119,45 +112,29 @@ def get_ddb_alarms_to_create(ddb_tables, aws_cw_connect, sns_topic_arn):
     assert isinstance(sns_topic_arn, str)
 
     alarms_to_create = set()
-    alarms_to_update = set()
     existing_alarms = get_existing_alarm_names(aws_cw_connect)
 
     for table in ddb_tables:
         # we want two alarms per DynamoDB table
         for metric in DDB_METRICS:
-            if metric == u'ConsumedReadCapacityUnits':
-                threshold = table[1]
-            elif metric == u'ConsumedWriteCapacityUnits':
-                threshold = table[2]
-            # initiate a MetricAlarm object for each DynamoDb table.
-            # for the threshold we calculate the 80 percent
-            # from the tables ProvisionedThroughput values
             ddb_table_alarm = MetricAlarm(
-                name=u'{}-{}-BasicAlarm'.format(table[0],
-                                                (metric.
-                                                 replace('Consumed',
-                                                         '') + 'Limit')),
+                name=u'{}-{}-BasicAlarm'.format(
+                    table, metric),
                 namespace=u'AWS/DynamoDB',
                 metric=u'{}'.format(metric), statistic='Sum',
                 comparison=u'>=',
-                threshold=0.8*threshold*ALARM_PERIOD,
+                threshold=0,
                 period=ALARM_PERIOD,
                 evaluation_periods=ALARM_EVALUATION_PERIOD,
                 # Below insert the actions appropriate.
                 alarm_actions=[sns_topic_arn],
-                dimensions={u'TableName': table[0]})
+                dimensions={u'TableName': table})
 
             # we create an Alarm metric for each new DDB table
-            if ddb_table_alarm.name not in existing_alarms.iterkeys():
+            if ddb_table_alarm.name not in existing_alarms:
                 alarms_to_create.add(ddb_table_alarm)
-            # checking the existing alarms thresholds
-            # update them if there are changes
-            for key, value in existing_alarms.iteritems():
-                if (key == ddb_table_alarm.name
-                        and value != ddb_table_alarm.threshold):
-                    alarms_to_update.add(ddb_table_alarm)
 
-    return (alarms_to_create, alarms_to_update)
+    return alarms_to_create
 
 
 def main():
@@ -175,11 +152,9 @@ def main():
     ddb_tables = get_ddb_tables(region)
     aws_cw_connect = boto.ec2.cloudwatch.connect_to_region(region)
 
-    (alarms_to_create,
-     alarms_to_update) = get_ddb_alarms_to_create(ddb_tables,
-                                                  aws_cw_connect,
-                                                  sns_topic_arn)
-
+    alarms_to_create = get_ddb_alarms_to_create(ddb_tables,
+                                                aws_cw_connect,
+                                                sns_topic_arn)
     # Creating new alarms
     if alarms_to_create:
         if DEBUG:
@@ -190,17 +165,6 @@ def main():
             for alarm in alarms_to_create:
                 print alarm
                 aws_cw_connect.create_alarm(alarm)
-
-    # Updating existing alarms
-    if alarms_to_update:
-        if DEBUG:
-            for alarm in alarms_to_update:
-                print 'DEBUG UPDATED:', alarm
-        else:
-            print 'DynamoDB table(s) Alarms updated:'
-            for alarm in alarms_to_update:
-                print alarm
-                aws_cw_connect.update_alarm(alarm)
 
 if __name__ == '__main__':
 
